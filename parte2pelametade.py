df9d4fc8d7dfb9c9daf72d4e57baaebfa74492df
gabrielmaced <105677830+gabrielmaced@users.noreply.github.com>
2024-04-09 11:57:50 -0300

    PARTE2 SEGUNDA AULA

--- parte2pelametade.py
+++ parte2pelametade.py
@@ -19,38 +19,161 @@ def main():
 
 def declarations(lista):
     consome(lista,'var')
-    declaration()
+    declaration(lista)
+    restoDeclaration(lista)
 
 def declaration(lista):
     listaIdent()
+    consome(lista,obter_valor_simbolo('tkn_doispontos'))
+    typeFuncao(lista)
+    consome(lista,obter_valor_simbolo('tkn_pontoevirgula'))
 
 def listaIdent(lista):
     consome(lista,obter_valor_simbolo('tkn_variaveis'))
-    restoIdentList()
+    restoIdentList(lista)
 
 def restoIdentList(lista):
     consome(lista,obter_valor_simbolo('tkn_virgula'))
     consome(lista,obter_valor_simbolo('tkn_variaveis'))
-    restoIdentList()
+    restoIdentList(lista)
 
-def type1(lista):
-    consome(lista,3)
-    consome(lista,4)
-    consome(lista,5)
+    # OU
+    # VAZIO
+
+def restoDeclaration(lista):
+    declaration(lista)
+    restoDeclaration(lista)
+
+    # OU
+    # VAZIO
+
+def typeFuncao(lista):
+    consome(lista,obter_valor_simbolo('integer'))
+    # OU
+    consome(lista,obter_valor_simbolo('real'))
+    # OU
+    consome(lista,obter_valor_simbolo('string'))
+
+# ------------------------------------------------
+# INSTRUÇÕES DOS PROGRAMAS
+
+def bloco(lista):
+    consome(lista,obter_valor_simbolo('begin'))
+    stmList(lista)
+    consome(lista,obter_valor_simbolo('end'))
 
 def stmList(lista):
-    stmt()
-    stmList()
+    stmt(lista)
+    stmList(lista)
+
+    # OU
+    # VAZIO
+
 
 def stmt(lista):
-    forStmt()
-
-    ioStmt()
-
-    whileStmt()
-
-    atrib()
+    forStmt(lista)
+    # OU
+    ioStmt(lista)
+    # OU
+    whileStmt(lista)
+    # OU
+    atrib(lista)
     consome('tkn_pontoevirgula')
+    # OU
+    ifStmt(lista)
+    # OU
+    bloco(lista)
+    # OU
+    consome(lista,obter_valor_simbolo('break'))
+    consome(lista,obter_valor_simbolo('tkn_pontoevirgula'))
+    # OU
+    consome(lista,obter_valor_simbolo('continue'))
+    consome(lista,obter_valor_simbolo('tkn_pontoevirgula'))
+    # OU
+    consome(lista,obter_valor_simbolo('tkn_pontoevirgula'))
+
+# ----------------------------------------------------------------------------
+# DESCRIÇÃO AS INSTRUCOES
+    
+def forStmt(lista):
+    consome(lista,obter_valor_simbolo('for'))
+    atrib(lista)
+    consome(lista,obter_valor_simbolo('to'))
+    endFor(lista)
+    consome(lista,obter_valor_simbolo('do'))
+    stmt(lista)
+
+def endFor(lista):
+    consome(lista,obter_valor_simbolo('tkn_variaveis'))
+    # OU
+    consome(lista,obter_valor_simbolo('tkn_int'))
+
+def ioStmt(lista):
+    consome(lista,obter_valor_simbolo('read'))
+    consome(lista,obter_valor_simbolo('tkn_abreparentese'))
+    consome(lista,obter_valor_simbolo('tkn_variaveis'))
+    consome(lista,obter_valor_simbolo('tkn_fechaparentese'))
+    consome(lista,obter_valor_simbolo('tkn_ponoevirula'))
+    # OU
+    consome(lista,obter_valor_simbolo('write'))
+    consome(lista,obter_valor_simbolo('tkn_abreparentese'))
+    out(lista)
+    consome(lista,obter_valor_simbolo('tkn_fechaparentese'))
+    consome(lista,obter_valor_simbolo('tkn_ponoevirula'))
+
+def out(lista):
+    consome(lista,obter_valor_simbolo('tkn_string'))
+    # OU
+    consome(lista,obter_valor_simbolo('tkn_variaveis'))
+    # OU
+    consome(lista,obter_valor_simbolo('tkn_int'))
+    # OU
+    consome(lista,obter_valor_simbolo('tkn_float'))
+
+def whileStmt(lista):
+    consome(lista,obter_valor_simbolo('while'))
+    expr(lista)
+    stmt(lista)
+
+def ifStmt(lista):
+    consome(lista,obter_valor_simbolo('if'))
+    expr(lista)
+    consome(lista,obter_valor_simbolo('then'))
+    stmt(lista)
+    elsePart(lista)
+
+def elsePart(lista):
+    consome(lista,obter_valor_simbolo('else'))
+    stmt(lista)
+    # OU
+    # VAZIO
+
+# ------------------------------------------------------------------------
+# expressoes
+    
+def atrib(lista):
+    consome(lista,obter_valor_simbolo('tkn_variaveis'))
+    consome(lista,obter_valor_simbolo('tkn_atribuicao'))
+    expr(lista)
+
+def expr(lista):
+    orFuncao(lista)
+
+def orFuncao(lista):
+    andFuncao(lista)
+    restoOr(lista)
+
+def restoOr(lista):
+    consome(lista,obter_valor_simbolo('or'))
+    andFuncao(lista)
+    restoOr(lista)
+    # OU
+    # VAZIO
+
+    
+
+
+
 
 # CADA CONSOME DA UM POP NA LISTA
 def consome(lista,token_consumido):
